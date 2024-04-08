21f9e9b51d967d8123a79b3e625136e21d0e457e
Ishita Sharma <isharma2@academic.rrc.ca>
2024-04-08 01:21:05 -0500

    Updated categories and products for pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,11 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @category = Category.find(params[:id])
-    @products = @category.products
+    @products = @category.products.paginate(page: params[:page], per_page: 10)
+
   end
 end
