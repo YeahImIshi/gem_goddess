dd9abeebe360990574a9bef13ed4903b209104dd
Ishita Sharma <isharma2@academic.rrc.ca>
2024-04-07 21:18:07 -0500

    Created pagecontroller for about and contact and respective erbs

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
    namespace :admin do
     get 'dashboard', to: 'dashboard#index'
   end
+  get '/about', to: 'pages#about', as: 'about'
+get '/contact', to: 'pages#contact', as: 'contact'
+get '/categories', to: 'categories#index', as: 'categories'
 
   get '/products/category/:category_id', to: 'products#category', as: 'products_by_category'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
